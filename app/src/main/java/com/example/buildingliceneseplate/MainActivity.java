6a9ddbf3386df36c9e14d389553b934c30e05ffd
박기수 <rltn2121@naver.com>
2020-02-07 16:37:53 +0900

    gisu notebook

--- app/src/main/java/com/example/buildingliceneseplate/MainActivity.java
+++ app/src/main/java/com/example/buildingliceneseplate/MainActivity.java
@@ -4,13 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.TextView;
-
+// git test 
 public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
-        //d
+
     }
 
     @Override
